6f4ada1bf8584f209079fc928abd75ac40ee73b5
Stephanie Datu <stephdatu@gmail.com>
2013-08-22 12:45:55 -0700

    Setting up Redis To Go for deployment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PhlogBlog::Application.config.secret_key_base = '44618799b36b16996a4292e86981eabf29a8c412262b0d77fce66cb453da8c7718d3249f25e715ceda8f400a248b3f7eddca48a153b86fa585d5c51a85b7c98b'
+PhlogBlog::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
